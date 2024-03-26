01cbcdb395cee41300fcd076612e1550b92bbb09
legoraystudios <49772068+legoraystudios@users.noreply.github.com>
2024-03-26 13:36:13 -0400

    Improved the ListView of InventoryView.

--- EZAvailability/Services/ProductService.cs
+++ EZAvailability/Services/ProductService.cs
@@ -15,11 +15,11 @@ namespace EZAvailability.Services
     {
         public ProductService() { }
 
-        public async static Task<ResponseData> GetProducts()
+        public async static Task<ResponseData> GetProducts(int limitPerPage, int page)
         {
 
             // Set the API Endpoint to make the request
-            string endpoint = ConnectionViewModel.ConnectionUrl + "/products/";
+            string endpoint = ConnectionViewModel.ConnectionUrl + "/products?limitPerPage=" + limitPerPage + "&page=" + page;
             
             // Check if the URL Exist in ConnnectionUrl
             if (ConnectionViewModel.ConnectionUrl == "" || ConnectionViewModel.ConnectionUrl == "Not Found")
@@ -62,5 +62,151 @@ namespace EZAvailability.Services
                 }
             }
         }
+
+        public async static Task<ResponseData> GetProductsByCategoryId(int categoryId, int limitPerPage, int page)
+        {
+
+            // Set the API Endpoint to make the request
+            string endpoint = ConnectionViewModel.ConnectionUrl + "/products?limitPerPage=" + limitPerPage + "&page=" + page + "&categoryId=" + categoryId;
+
+            // Check if the URL Exist in ConnnectionUrl
+            if (ConnectionViewModel.ConnectionUrl == "" || ConnectionViewModel.ConnectionUrl == "Not Found")
+            {
+                return new ResponseData { StatusCode = -1, JsonResponse = null };
+            }
+
+            // Set the HTTP Handler
+            var handler = BaseService.DefaultHttpClientHandler();
+
+            // Set the Cookie Container in order to set the required
+            // session cookies to access the protected content
+            var cookieContainer = handler.CookieContainer;
+
+            using (HttpClient client = new HttpClient(handler))
+            {
+                // Setting the session cookies (refresh and access
+                // token) to be able to access to this protected
+                // endpoint
+                foreach (var cookie in SessionManager.Cookies)
+                {
+                    cookieContainer.SetCookies(new Uri(endpoint), cookie);
+                }
+
+                // Process the API request
+                HttpResponseMessage apiResponse = await client.GetAsync(endpoint);
+                // Obtain the JSON Response
+                string jsonResponse = await apiResponse.Content.ReadAsStringAsync();
+
+
+                if (apiResponse.IsSuccessStatusCode)
+                {
+                    // Return OK Status and products data if the request was success
+                    return new ResponseData { StatusCode = 200, JsonResponse = jsonResponse };
+                }
+                else
+                {
+                    // Request was not successful
+                    return new ResponseData { StatusCode = (int)apiResponse.StatusCode, JsonResponse = jsonResponse };
+                }
+            }
+        }
+
+        public async static Task<ResponseData> GetProductsById(int id)
+        {
+
+            // Set the API Endpoint to make the request
+            string endpoint = ConnectionViewModel.ConnectionUrl + "/products/" + id;
+
+            // Check if the URL Exist in ConnnectionUrl
+            if (ConnectionViewModel.ConnectionUrl == "" || ConnectionViewModel.ConnectionUrl == "Not Found")
+            {
+                return new ResponseData { StatusCode = -1, JsonResponse = null };
+            }
+
+            // Set the HTTP Handler
+            var handler = BaseService.DefaultHttpClientHandler();
+
+            // Set the Cookie Container in order to set the required
+            // session cookies to access the protected content
+            var cookieContainer = handler.CookieContainer;
+
+            using (HttpClient client = new HttpClient(handler))
+            {
+                // Setting the session cookies (refresh and access
+                // token) to be able to access to this protected
+                // endpoint
+                foreach (var cookie in SessionManager.Cookies)
+                {
+                    cookieContainer.SetCookies(new Uri(endpoint), cookie);
+                }
+
+                // Process the API request
+                HttpResponseMessage apiResponse = await client.GetAsync(endpoint);
+                // Obtain the JSON Response
+                string jsonResponse = await apiResponse.Content.ReadAsStringAsync();
+
+
+                if (apiResponse.IsSuccessStatusCode)
+                {
+                    // Return OK Status and products data if the request was success
+                    return new ResponseData { StatusCode = 200, JsonResponse = jsonResponse };
+                }
+                else
+                {
+                    // Request was not successful
+                    return new ResponseData { StatusCode = (int)apiResponse.StatusCode, JsonResponse = jsonResponse };
+                }
+            }
+        }
+
+        public async static Task<ResponseData> GetProductByName(string name, int limitPerPage, int page)
+        {
+
+            // Set the API Endpoint to make the request
+            string endpoint = ConnectionViewModel.ConnectionUrl + "/products?limitPerPage=" + limitPerPage + "&page=" + page + "&productName=" + name;
+
+            // Check if the URL Exist in ConnnectionUrl
+            if (ConnectionViewModel.ConnectionUrl == "" || ConnectionViewModel.ConnectionUrl == "Not Found")
+            {
+                return new ResponseData { StatusCode = -1, JsonResponse = null };
+            }
+
+            // Set the HTTP Handler
+            var handler = BaseService.DefaultHttpClientHandler();
+
+            // Set the Cookie Container in order to set the required
+            // session cookies to access the protected content
+            var cookieContainer = handler.CookieContainer;
+
+            using (HttpClient client = new HttpClient(handler))
+            {
+                // Setting the session cookies (refresh and access
+                // token) to be able to access to this protected
+                // endpoint
+                foreach (var cookie in SessionManager.Cookies)
+                {
+                    cookieContainer.SetCookies(new Uri(endpoint), cookie);
+                }
+
+                // Process the API request
+                HttpResponseMessage apiResponse = await client.GetAsync(endpoint);
+                // Obtain the JSON Response
+                string jsonResponse = await apiResponse.Content.ReadAsStringAsync();
+
+
+                if (apiResponse.IsSuccessStatusCode)
+                {
+                    // Return OK Status and products data if the request was success
+                    return new ResponseData { StatusCode = 200, JsonResponse = jsonResponse };
+                }
+                else
+                {
+                    // Request was not successful
+                    return new ResponseData { StatusCode = (int)apiResponse.StatusCode, JsonResponse = jsonResponse };
+                }
+            }
+        }
+
+
     }
 }
--- EZAvailability/Views/InventoryView.xaml.cs
+++ EZAvailability/Views/InventoryView.xaml.cs
@@ -1,20 +1,36 @@
 using EZAvailability.Data;
 using EZAvailability.Services;
+using EZAvailability.ViewModel;
 using EZAvailability.ViewModel.Base;
 using Newtonsoft.Json;
+using System.Collections.ObjectModel;
 using System.Security.Cryptography.X509Certificates;
 namespace EZAvailability.Views;
 
 public partial class InventoryView : ContentPage
 {
-
+    // Initializing ViewModel
     private BaseViewModel baseViewModel = new BaseViewModel();
+    private ProductViewModel productViewModel = new ProductViewModel();
+
+    // Product Lists
+    List<ProductData> productInfo;
     public List<ProductData> Products { get; set; }
+
+    // Pagination variables for product ListView
+    private static int limitPerPage = 10;
+    private int page = 1;
+
+    // Variables for the Search Bar
+    private string SearchValue {  get; set; }
+    private int SelectedSearchType;
 
     public InventoryView()
 	{
 		InitializeComponent();
-        this.BindingContext = baseViewModel;
+        LoadingModule.BindingContext = baseViewModel;
+
+        ProductList.Scrolled += OnProductListViewScrolled;
     }
 
     protected override async void OnAppearing()
@@ -34,16 +50,39 @@ public partial class InventoryView : ContentPage
 
     private async Task LoadProducts()
     {
-        List<ProductData> productInfo = await GetProducts();
+        BindingContext = null;
+        productInfo = await GetProducts();
         Products = productInfo;
         BindingContext = this;
     }
+
+
 
     private async Task<List<ProductData>> GetProducts()
     {
         try
         {
-            ResponseData response = await ProductService.GetProducts();
+            if (SearchValue != null) // If the Search Bar is NOT NULL
+            {
+                if (SelectedSearchType == 0) // If Search Type is by Product Name
+                {
+
+                } else if (SelectedSearchType == 1) // If Search Type is by Product ID
+                {
+
+                } else if (SelectedSearchType == 2) // If Search Type is by Product Name
+                {
+
+                } else // Get Products
+                {
+
+                }
+            } else // Get Products
+            {
+
+            }
+
+            ResponseData response = await ProductService.GetProducts(limitPerPage, page);
             string jsonResponse = response.JsonResponse;
 
             List<ProductData> productData = JsonConvert.DeserializeObject<List<ProductData>>(jsonResponse);
@@ -59,6 +98,53 @@ public partial class InventoryView : ContentPage
         {
             await DisplayAlert("Error", "An error has occured while loading the products. Please, try again later." + ex, "Okay");
             return null;
+        }
+
+    }
+
+    private async void OnProductListViewScrolled(object sender, ScrolledEventArgs e)
+    {
+        var scrollView = sender as ScrollView;
+
+        // Check if the sender object is null or if loading is already in progress
+        if (scrollView == null || baseViewModel.isLoading)
+            return;
+
+        var scrollY = e.ScrollY;
+        var contentHeight = scrollView.ContentSize.Height; // Use ContentSize to get the total height of the content
+        var scrollViewHeight = scrollView.Height;
+
+        // If the content height is not available or the scroll view height is zero, return
+        if (contentHeight <= 0 || scrollViewHeight <= 0)
+            return;
+
+        // Calculate the ratio of how far the user has scrolled
+        var scrollRatio = scrollY / (contentHeight - scrollViewHeight);
+
+        // If the scroll ratio is greater than or equal to 80%, trigger loading more products
+        if (scrollRatio >= 0.8)
+        {
+            baseViewModel.isLoading = true;
+            page++; // Increment the page number for pagination
+            await LoadNextPageProducts();
+            baseViewModel.isLoading = false;
+        }
+    }
+
+    private async Task LoadNextPageProducts()
+    {
+        // Call your method to fetch the next page of products
+        List<ProductData> nextPageProducts = await GetProducts();
+
+        if (nextPageProducts.Count > 0)
+        {
+            baseViewModel.isLoading = true;
+            productInfo.AddRange(nextPageProducts);
+
+            BindingContext = null;
+            BindingContext = this;
+            OnPropertyChanged(nameof(Products));
+            baseViewModel.isLoading = false;
         }
 
     }
@@ -86,5 +172,15 @@ public partial class InventoryView : ContentPage
             return null;
         }
 
+    }
+
+    private void SearchBtn_Clicked(object sender, EventArgs e)
+    {
+        SearchValue = SearchValue_Entry.Text;
+    }
+
+    private void OnSearchTypeSelected(object sender, EventArgs e)
+    {
+        SelectedSearchType = ((Picker)sender).SelectedIndex;
     }
 }
